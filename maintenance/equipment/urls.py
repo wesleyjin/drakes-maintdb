345197937694b1d2e48e4e355521511c4f76b021
Wesley Jin <wesleyjin12@berkeley.edu>
2018-04-16 14:59:53 -0700

    updated equipment views

--- maintenance/equipment/urls.py
+++ maintenance/equipment/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# /equipment/
     path('', views.index, name='index'),
+    # /equipment/bh1/
+    path('<str:location_id>/', views.location, name = 'location'),
+    # /equipment/kettle1/
+    path('<int:equipment_id>/', views.info, name = 'info')
 ]
--- maintenance/equipment/views.py
+++ maintenance/equipment/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the equipment index.")
+
+def location(request, location_id):
+	return HttpResponse("You're looking at location %s" % location_id)
+
+def info(request, equipment_id):
+	response = "You're looking at the info page of equipment %s"
+	return HttpResponse(response % equipment_id)
