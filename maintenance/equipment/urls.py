c3076c5512157cc3d282ec688ddf7a8799315ddb
Wesley Jin <wesleyjin12@berkeley.edu>
2018-04-19 17:46:34 -0700

    add view for equipment contained in a location

--- maintenance/equipment/urls.py
+++ maintenance/equipment/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# /equipment/
-    path('', views.index, name='index'),
-    # /equipment/bh1/
+	path('', views.index, name='index'),
+	# /equipment/locations
+    path('locations/', views.plant_locations, name='plant_locations'),
+    # /equipment/BH1/
     path('<str:location_id>/', views.location, name = 'location'),
     # /equipment/bh1/kettle1/
     path('<str:location_id>/<int:equipment_id>/', views.info, name = 'info')
--- maintenance/equipment/views.py
+++ maintenance/equipment/views.py
@@ -3,15 +3,19 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Location, Equipment, Part
 
-
 def index(request):
+	return HttpResponse("Equipment homepage. Navigate to equipment/locations to see what we have so far.")
+
+def plant_locations(request):
 	location_list = Location.objects.order_by('location_name')
 	context = {'plant_locations': location_list}
 	return render(request,'equipment/locationdash.html', context)
 
 def location(request, location_id):
 	loc = get_object_or_404(Location, pk = location_id)
-	return HttpResponse("You're looking at location %s" % loc.location_name)
+	equipment_list = list(Equipment.objects.filter(location = loc.location_id))
+	context = {'location': loc, 'equipments': equipment_list}
+	return render(request, 'equipment/locationinfo.html', context)
 
 def info(request, equipment_id):
 	response = "You're looking at the info page of equipment %s"
