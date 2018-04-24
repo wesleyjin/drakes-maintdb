949c574767bc909f09dc7dbab6bf1d179e7306f0
Wesley Jin <wesleyjin12@berkeley.edu>
2018-04-24 00:46:48 -0700

    migrate DB to have 'assigned_to' field in log

--- maintenance/logs/urls.py
+++ maintenance/logs/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 #logs app
-app_name = 'maintenance'
+app_name = 'logs'
 urlpatterns = [
     # /maintenance/
     path('', views.index, name='index'),
+    path('logs/', views.index),
+    path('services/', views.index)
 ]
